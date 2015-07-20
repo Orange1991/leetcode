2edb8b2548a90f4638692ce2cfca00bfa0b664a0
Orange <sfg1991@163.com>
2015-07-20 18:49:12 +0800

    127 java accept.

--- 127/cpp/main.cpp
+++ 127/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <unordered_set>
 #include <unordered_map>
 
@@ -7,16 +8,80 @@ using namespace std;
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, unordered_set<string>& dict) {      
+        /* 添加所有字符串到map */
+        unordered_map<string, int> map;
         int index = 0;
-  
-        dict.insert(beginWord);
-        dict.insert(endWord);
-        unordered_set<string>::iterator x = dict.begin(), y = dict.end();
-        for(; x != y; ++x)
-            cout << *x << " -> ";
+        string tmp;
+        map.insert(make_pair(beginWord, index++));
+        map.insert(make_pair(endWord, index++));
 
-        return dict.size();
+        if (dict.find(beginWord) != dict.end())
+            dict.erase(beginWord);
+        if (dict.find(endWord) != dict.end())
+            dict.erase(endWord);
+
+        for(unordered_set<string>::iterator it = dict.begin(), itEnd = dict.end(); it != itEnd; ++it) {
+            map.insert(make_pair(*it, index++));
+        }
+
+        /* 完成下标到字符串的映射 */
+        vector<string> list(map.size());
+        for (unordered_map<string, int>::iterator it = map.begin(), end = map.end(); it != end; ++it) {
+            cout << it->second << "-->" << it->first << endl;            
+            list[it->second] = it->first;
+        }
+// 往上都测试过了，没问题
+
+        /* 构造无向图的邻接表 */
+/*        int len = list.length;
+        vector<int> head = new vector<int>(len);
+        int indexOfArgNode = 0;
+        vector<int> args, p;
+        for (int i = 0; i < len; ++i) {
+            heads[i] = -1;
+        }
+        for (int i = 0; i < len - 1; ++i) {
+            for (int j = i + 1; j < len; ++j) {
+                if (diff(list[i], list[j]) == 1) {
+                    args.add(heads[i]);
+                    p.add(j);
+                    heads[i] = indexOfArgNode++;
+                    args.add(heads[j]);
+                    p.add(i);
+                    heads[j] = indexOfArgNode++;
+                }
+            }
+        }
+  */      
+        /* 广度优先遍历 */
+  /*      int queue[] = new int[len], queCur = 0, queEnd = 0;
+        boolean[] visited = new boolean[len];
+        int[] distance = new int[len];
+        for (int i = 0; i < len; ++i) {
+            visited[i] = false;
+            distance[i] = 0;
+        }
+        queue[queEnd++] = 0;
+        visited[0] = true;
+        int node, target;
+        while (queCur < queEnd) {
+            node = heads[queue[queCur]];
+            while (node != -1) {
+                target = p.get(node);
+                if (!visited[target]) {
+                    visited[target] = true;
+                    queue[queEnd++] = target;
+                    distance[target] = distance[queue[queCur]] + 1;
+                }
+                node = args.get(node);
+            }
+            ++queCur;
+        }
+
+        return visited[1] ? distance[1] + 1 : 0;
+*/return 0;
     }
+
 };
 
 int main(void) {
@@ -29,6 +94,6 @@ int main(void) {
     dict.insert("log");
 
     Solution s;
-    cout << s.ladderLength(startWord, endWord, dict) << endl;;
+    cout << s.ladderLength(startWord, endWord, dict) << endl;
 
 }
--- 127/java/Solution.java
+++ 127/java/Solution.java
@@ -1,27 +1,83 @@
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.HashMap;
 
 public class Solution {
+
     public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        int index = 0;
-        map.put(beginWord, ++index);
-        map.put(endWord, ++index);
-        for (Iterator<String> it = wordDict.iterator(); it.hasNext();)
-            map.put(it.next(), ++index);
-        
-        Iterator it = map.keySet().iterator();
-        String tmp;
-        while (it.hasNext()) {
-            tmp = it.next().toString();
-            System.out.println(tmp + " -> " + map.get(tmp));
+        if (beginWord == null || endWord == null || beginWord.equals(endWord) || beginWord.length() != endWord.length())
+            return 0;
+
+        if (isOneCharDiff(beginWord, endWord))
+            return 2;
+
+        /* 广度优先遍历 
+         * 遍历到的新点入队列queue
+         * 同时将源点beginWord到新点的距离存在visited中 */
+        Queue<String> queue = new LinkedList<String>();
+        queue.add(beginWord);
+        HashMap<String, Integer> visited = new HashMap<String, Integer>();
+        visited.put(beginWord, 1);
+         
+        String tmp, sbStr;
+        int len, distance;
+        while (!queue.isEmpty()) {
+            // 获取队首元素
+            tmp = queue.poll();
+            // 依次将字符串的某一位替换为a-z中的字母
+            len = tmp.length();
+            for (int i = 0; i < len; ++i) {
+                for (char c = 'a'; c <= 'z'; ++c) {
+                    // 如果替换后字符串没有变化，则跳过此次尝试
+                    if (tmp.charAt(i) == c) continue;
+
+                    // 拷贝原字符串，并修改某一位为某一个字符
+                    StringBuilder sb = new StringBuilder(tmp);
+                    sb.setCharAt(i, c);
+                    sbStr = sb.toString();
+                    distance = visited.get(tmp) + 1; // 从visited中获取beginWord到tmp的距离并加1
+
+                    // 如果新字符串就是endWord，返回距离
+                    if (sbStr.equals(endWord))
+                        return distance;
+
+                    // 如果字典中有这个字符串，并且这个字符串是第一次被遍历到
+                    // 则将其作为新字符串添加到队列queue中，并添加到visited中&记录距离
+                    if (wordDict.contains(sbStr) && !visited.containsKey(sbStr)) {
+                        queue.add(sbStr);
+                        visited.put(sbStr, distance);
+                    }
+                }
+            }
         }
-        return map.size();
+
+        // 执行至此，说明图为非连通图，beginWord和endWord之间不能转换
+        return 0;
     }
     
+    /**
+     * 判断两个字符串的差异度是否为1
+     * 字符串长度相同，比较每一位字母，判断差异度
+     * @param str1 字符串1
+     * @param str2 字符串2
+     * @return 两字符串差异度为1则返回true，否则返回false
+     */
+    private boolean isOneCharDiff(String str1, String str2) {
+        int count = 0, len = str1.length();
+        for (int i = 0; i < len; ++i) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                ++count;
+                if (count > 1)
+                    return false;
+            }
+        }
+
+        return count == 1;
+    }
+
     public static void main(String[] args) {
         String startWord = "hit", endWord = "cog";
         Set<String> wordDict = new HashSet<String>();
@@ -30,7 +86,13 @@ public class Solution {
         wordDict.add("dog");
         wordDict.add("lot");
         wordDict.add("log");
-        new Solution().ladderLength(startWord, endWord, wordDict);
-        System.out.println("OK");
+        System.out.println(new Solution().ladderLength(startWord, endWord, wordDict));
+        
+
+        Set<String> dict = new HashSet<String>();
+		dict.add("a");
+        dict.add("b");
+        dict.add("c");
+        System.out.println(new Solution().ladderLength("a", "b", dict));
     }
 }
