52fa5ecc0d637b51eddc3f93f6d6fc7c487893df
sfg1991 <sfg1991@163.com>
2015-08-17 22:47:15 +0800

    260 Single Number III init

---
+++ 260/README.md
@@ -0,0 +1,32 @@
+#
+
+##原题地址
+https://leetcode.com/problems/single-number-iii/
+
+##题目描述
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+给出一个数组，其中只有两个元素仅出现一次，其余元素均出现两次。找出只出现一次的两个元素。
+
+For example:
+例如：
+
+Given `nums = [1, 2, 1, 3, 2, 5]`, return `[3, 5]`.
+给出`nums = [1, 2, 1, 3, 2, 5]`，返回`[3, 5]`。
+
+Note:
+注意：
+
+The order of the result is not important. So in the above example, `[5, 3]` is also correct.
+结果的顺序无关紧要。所以上述例子中`[5, 3]`也是正确的。
+
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant space complexity?
+你的算法需要在线性时间复杂度内运行。你可以在常数空间复杂度内实现吗？要求：**时间复杂度O(n)**，**空间复杂度O(1)**。
+
+##解题思路
+这个题目是SingleNumber系列的第三题，同系列的题目请看如下博文：
+
+> 1. [Leetcode 136 Single Number 仅出现一次的数字](http://blog.csdn.net/smile_watermelon/article/details/47733979)
+> 2. [Leetcode 137 Single Number II ]
+> 3. [有一个数组data[n-1]存储了1~n中的n-1个数，问data中缺少的数字是多少【每日一题】](http://blog.csdn.net/smile_watermelon/article/details/45194901)
+
+
---
+++ 260/c/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* singleNumber(int* nums, int numsSize, int* returnSize) {
+    int* ret = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    
+    // a: 目标结果a
+    // b: 目标结果b
+    // tmp: a^b的结果
+    // one: a和b最低的不相同的位
+    int tmp = 0, a = 0, b = 0, i, one = 1;
+    for (i = 0; i < numsSize; ++i) // 计算a^b
+        tmp ^= *(nums + i);
+    while (!(tmp & 1)) { // 获取a和b最低的不相同的位
+        one <<= 1;
+        tmp >>= 1;
+    }
+    // 按a和b最低的不相同的位把所有数据分为两组
+    // a和b分别分在两个组里，问题转化为两个2n+1的问题
+    // 两组分别异或即可得到两个目标结果
+    for (i = 0; i < numsSize; ++i) 
+        if (one & *(nums + i))
+            a ^= *(nums + i);
+        else
+            b ^= *(nums + i);
+    *ret = a;
+    *(ret + 1) = b;
+    return ret;
+}
+
+void test(int* nums, int numsSize) {
+    int i, size;
+    printf("    [");
+    for (i = 0; i < numsSize - 1; ++i)
+        printf("%d,", *(nums + i));
+    int* ret = singleNumber(nums, numsSize, &size);
+    printf("%d] : [%d,%d]\n", *(nums + numsSize - 1), *ret, *(ret + 1));
+    free(ret);
+}
+int main() {
+    int nums[] = {1, 2, 1, 3, 5, 2};
+    test(nums, 6);
+}
