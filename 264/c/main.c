2f8d2b2586d40615917511c02377c4565d8efcb3
Orange <sfg1991@163.com>
2015-09-01 12:48:34 +0800

    264 Ugly Number II

--- 264/c/main.c
+++ 264/c/main.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * 获取第n个ugly数字
+ */
 int nthUglyNumber(int n) {
-    void mergeToUgly(int*, int*, int, int*, int*, int*, int*, int*, int*);
+    void merge(long long*, int*, int, long long*, int*, long long*, int*, long long*, int*);
     if (n <= 6) return n;
-    int* ugly = (int*)malloc(sizeof(int) * n * 5);
-    int* l2 = (int*)malloc(sizeof(int) * n);
-    int* l3 = (int*)malloc(sizeof(int) * n);
-    int* l5 = (int*)malloc(sizeof(int) * n);
-
+    long long* ugly = (long long*)malloc(sizeof(long long) * 2 * n);
+    long long* l2 = (long long*)malloc(sizeof(long long) * n);
+    long long* l3 = (long long*)malloc(sizeof(long long) * n);
+    long long* l5 = (long long*)malloc(sizeof(long long) * n);
+    int count = 1, end = 0, p2 = 0, p3 = 0, p5 = 0;
     *ugly = 1;
-    int count = 1, p2 = 0, p3 = 0, p5 = 0, cur = 0, l2max;
     while (count < n) {
-        for (; cur < count; ++cur) {
-            *(l2 + cur) = *(ugly + cur) * 2;
-            *(l3 + cur) = *(ugly + cur) * 3;
-            *(l5 + cur) = *(ugly + cur) * 5;
+        while (end < count) {   // 尝试将已有的有序的ugly数字分别乘以2/3/5，来扩展ugly数字候选列表
+            *(l2 + end) = *(ugly + end) * 2;    // 以*2的方式扩展
+            *(l3 + end) = *(ugly + end) * 3;    // 以*3的方式扩展
+            *(l5 + end) = *(ugly + end) * 5;    // 以*5的方式扩展
+            ++end;
         }
-        l2max = *(l2 + cur - 1);
-        mergeToUgly(ugly, &count, l2max, l2, &p2, l3, &p3, l5, &p5);
-printf("%d %d\n", count, *(ugly + count - 1));
+        // 将备选列表中的数字进行有序归并
+        merge(ugly, &count, end, l2, &p2, l3, &p3, l5, &p5);    
     }
 
     int ans = *(ugly + n - 1);
@@ -27,39 +29,30 @@ printf("%d %d\n", count, *(ugly + count - 1));
     free(l2);
     free(l3);
     free(l5);
-
     return ans;
 }
 
-void mergeToUgly(int* ugly, int* count, int max, int* l2, int* p2, int* l3, int* p3, int* l5, int* p5) {
-    int end = *count;
-if (*count == 1545)
-    printf("%lld, %lld, %lld\n", *(l2 + *p2), *(l3 + *p3), *(l5 + *p5));
-    while (*p2 < end && *(l2 + *p2) <= max) {
-        if (*(l2 + *p2) <= *(l3 + *p3) && *(l2 + *p2) <= *(l5 + *p5)) { // l2[p2] is the minimum.
-            if (*(l2 + *p2) == *(ugly + *count - 1)) {
-                ++(*p2);
-                continue;
-            }
-            *(ugly + (*count)++) = *(l2 + (*p2)++);    // add the count-th ugly number.
-        } else if (*(l3 + *p3) <= *(l5 + *p5)) {    // l3[p3] is the minimum.
-            if (*(l3 + *p3) == *(ugly + *count - 1)) {
-                ++(*p3);
-                continue;
-            }
-            *(ugly + (*count)++) = *(l3 + (*p3)++);    // add the count-th ugly number.
-        } else {    // l5[p5] is the minimum.
-            if (*(l5 + *p5) == *(ugly + *count - 1)) {
-                ++(*p5);
-                continue;
-            }
-            *(ugly + (*count)++) = *(l5 + (*p5)++);    // add the count-th ugly number.
-        }
-    }
+/**
+ * 将三个列表中的ugly数字有序的归并添加到ugly中
+ */
+void merge(long long* ugly, int* count, int end, long long* l2, int* p2, long long* l3, int* p3, long long* l5, int* p5) {
+    long long max = *(l2 + end - 1);    // l2末尾元素，l2/l3/l5中小于此元素的均可以有序的添加到ugly中
+    while (*p2 < end && *(l2 + *p2) <= max) 
+        if (*(l2 + *p2) <= *(l3 + *p3) && *(l2 + *p2) <= *(l5 + *p5))  // l2中的当前结点最小
+            if (*(l2 + *p2) == *(ugly + *count - 1)) (*p2)++;   // 略过重复元素
+            else *(ugly + (*count)++) = *(l2 + (*p2)++);        // 从l2中添加下一个ugly数字
+        else if (*(l3 + *p3) <= *(l5 + *p5))  // l3中的当前结点最小
+            if (*(l3 + *p3) == *(ugly + *count - 1)) (*p3)++;   // 略过重复元素
+            else *(ugly + (*count)++) = *(l3 + (*p3)++);        // 从l3中添加下一个ugly数字
+        else     // l5中的当前结点最小
+            if (*(l5 + *p5) == *(ugly + *count - 1)) (*p5)++;   // 略过重复元素
+            else *(ugly + (*count)++) = *(l5 + (*p5)++);        // 从l5中添加下一个ugly数字
 }
 
 int main() {
-    printf("%d : %d\n", 20, nthUglyNumber(20));
+    int i;
+    for (i = 1; i < 101; ++i)
+        printf("%d : %d\n", i, nthUglyNumber(i));
     printf("%d : %d\n", 1600, nthUglyNumber(1600));
     return 0;
 }
